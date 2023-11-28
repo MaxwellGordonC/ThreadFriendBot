1728b13f96e2408344367b50a55af11a4cf5bd05
MaxwellGordonC <56568996+MaxG-cmd@users.noreply.github.com>
2023-11-28 16:27:51 -0600

    Update config & GitIgnore

--- config/JSONReader.cs
+++ config/JSONReader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,19 @@ using System.Threading.Tasks;
 namespace ThreadFriendBot.config
 {
     internal class JSONReader
+    {
+        public string token { get; set; }
+
+        public async Task ReadJSON()
+        {
+            using (StreamReader sr = new StreamReader("config.json"))
+            {
+
+            }
+        }
+    }
+
+    internal sealed class JSONStructure
     {
         public string token { get; set; }
     }
