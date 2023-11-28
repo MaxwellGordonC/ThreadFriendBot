9f1f98c81d59d76597d9bb5910e096c68a80e5d0
MaxwellGordonC <56568996+MaxG-cmd@users.noreply.github.com>
2023-11-28 16:16:57 -0600

    Libraries

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadFriendBot
+{
+    internal class Program
+    {
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Commands { get; set; }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
