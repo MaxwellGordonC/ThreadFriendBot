328cb8a5a0648697db59c234595e3cd3cbb33033
MaxwellGordonC <56568996+MaxG-cmd@users.noreply.github.com>
2023-11-28 16:22:44 -0600

    SlashCommands Package

---
+++ config/JSONReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadFriendBot.config
+{
+    internal class JSONReader
+    {
+        public string token { get; set; }
+    }
+}
